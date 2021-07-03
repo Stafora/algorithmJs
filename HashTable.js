09f38b462585af6445d73f1757b79cc0b9dc4dde
staforkinartem <staforkinartem@yandex.ru>
2021-07-03 08:48:51 +0600

    master

---
+++ HashTable.js
@@ -0,0 +1,19 @@
+class HashTable {
+    store = new Array(10);
+    hash(key) {
+      let sum = 0;
+      
+      for (let i = 0; i < key.length; i++) {
+        sum += key.charCodeAt(i);
+      }
+      return sum % this.store.length;
+    }
+    add(key, value) {
+      this.store[this.hash(key)] = this.store[this.hash(key)] || [];
+      this.store[this.hash(key)].push({ key, value });
+    }
+    get(key) {
+      const item = (this.store[this.hash(key)] || []).find((item) => item.key === key);
+      return item && item.value;
+    }
+}
